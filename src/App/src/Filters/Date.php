3fdd18e6338eaf811dd5a1a427a73a731d343777
Einar Valur <fizk78@gmail.com>
2019-08-14 02:26:22 +1000

    Adds styles

--- src/App/src/Filters/Date.php
+++ src/App/src/Filters/Date.php
@@ -6,6 +6,35 @@ use Twig\TwigFilter;
 
 class Date extends AbstractExtension
 {
+    private $monthIs = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'Maí',
+        'Jún',
+        'Júl',
+        'Ágú',
+        'Sep',
+        'Okt',
+        'Nóv',
+        'Des',
+    ];
+    private $monthEn = [
+        'Jan',
+        'Feb',
+        'Mar',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Sep',
+        'Oct',
+        'Nov',
+        'Dec',
+    ];
+
     public function getFilters()
     {
         return [
@@ -13,8 +42,20 @@ class Date extends AbstractExtension
         ];
     }
 
-    public function date($date)
+    public function date($date, $language = 'is', $year = true)
     {
-        return (new \DateTime($date))->format('Y.m.d');
+        try {
+            $dateString = $year
+                ? (new \DateTime($date))->format('d. M Y')
+                : (new \DateTime($date))->format('d. M');
+            if ($language === 'is') {
+                $dateString = str_replace ($this->monthEn, $this->monthIs , $dateString);
+            }
+
+            return $dateString;
+        } catch (\Exception $e) {
+            return '';
+        }
+
     }
 }
